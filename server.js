3771ca21092fe5fcf622686277bf39bc04b5a23d
Deorren Erive <erive92d@gmail.com>
2023-01-14 16:01:34 -0800

    modified PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const path = require("path")
 const fs = require("fs")
 const dataBase = require("./db/db.json")
-const PORT = 3001
+const PORT = 3001 || process.env.PORT
 
 const app = express()
 
